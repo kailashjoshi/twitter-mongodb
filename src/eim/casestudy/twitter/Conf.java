94c515b9a5f0dd9c5c3228b40f2ba8363a19b0c7
kailashjoshi <kailashrajjoshi@gmail.com>
2013-09-25 00:10:41 -0600

    Update Conf.java

--- src/eim/casestudy/twitter/Conf.java
+++ src/eim/casestudy/twitter/Conf.java
@@ -8,9 +8,9 @@ public class Conf {
 	/**
 	 * Twitter Credentials
 	 */
-	protected static final String CONSUMER_KEY = "UcwAuM4xMIb5uX7YGD6lA";
-	protected static final String CONSUMER_SECRET = "jlpR5IeP3jUTeBAHumYtGF8wiIc9NspYhXl2tipuc";
-	protected static final String ACCESS_TOKEN = "1523025361-LLcTTGNozZoPjCgCCneHPCa7v9xmS13YPN6lfOU";
-	protected static final String ACCESS_TOKEN_SECRET = "wmu9Pa12pP8nEGThSS44wayOQNRAjjfAHzdZYeSlFA";	
+	protected static final String CONSUMER_KEY = "your_Consumer_key";
+	protected static final String CONSUMER_SECRET = "Your_consumer_seceret";
+	protected static final String ACCESS_TOKEN = "Your_Access_Token";
+	protected static final String ACCESS_TOKEN_SECRET = "Your_Access-Token_Secret";	
 
 }
